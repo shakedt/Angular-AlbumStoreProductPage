05743b161be79e2e0a2070b3e26df757d1c89969
shaked.tayeb <shaked.tayeb@ladbrokes.com>
2019-04-04 18:08:46 +0300

    updated the app.module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ProductService]
 })
 export class AppComponent {
   title = 'app works!';
